7c7d7d2938e8aa821d3e9229a26c1f759fe99d7b
crisri-mac <crifsri@icloud.com>
2024-01-20 22:33:33 +0800

    add swagger-ui-express

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 使用 DocumentBuilder来构建Swagger文档的基本信息。
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
+  //使用 SwaggerModule生成并注册了Swagger文档和UI。
   const document = SwaggerModule.createDocument(app, config, {});
   SwaggerModule.setup('api', app, document);
   // static 
